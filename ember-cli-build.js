06e2f63df212eda4f572b8ffebda7e9029e6c262
Christine Oo <ootjinvoon@gmail.com>
2015-07-21 17:02:38 +0900

    [UI] Add picnic and fontello into project

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -8,7 +8,24 @@ module.exports = function(defaults) {
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
-  //
+  app.import('bower_components/picnic/releases/plugins.min.css');
+  app.import('bower_components/picnic/releases/picnic.min.css');
+  app.import('vendor/fontello/fontello.css');
+  app.import('vendor/fontello/font/fontello.ttf', {
+    destDir: 'font'
+  });
+  app.import('vendor/fontello/font/fontello.eot', {
+    destDir: 'font'
+  });
+  app.import('vendor/fontello/font/fontello.ttf', {
+    destDir: 'font'
+  });
+  app.import('vendor/fontello/font/fontello.svg', {
+    destDir: 'font'
+  });
+  app.import('vendor/fontello/font/fontello.woff', {
+    destDir: 'font'
+  });
   // If you need to use different assets in different
   // environments, specify an object as the first parameter. That
   // object's keys should be the environment name and the values
